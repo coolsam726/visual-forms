7031dd287f5833ccc1e502c449a65195438f3566
Sam Maosa <smaosa@strathmore.edu>
2025-03-17 10:59:21 +0300

    New Feature: Fieldset - Added Fieldset to extend component - Configure columns for fieldset - Set label for fieldsets - Set fieldset as disabled

--- src/ComponentTypes/Fieldset.php
+++ src/ComponentTypes/Fieldset.php
@@ -2,4 +2,57 @@
 
 namespace Coolsam\VisualForms\ComponentTypes;
 
-class Fieldset {}
+use Coolsam\VisualForms\Utils;
+
+class Fieldset extends Component
+{
+    public function getOptionName(): string
+    {
+        return __('Fieldset');
+    }
+
+    public function isLayout(): bool
+    {
+        return true;
+    }
+
+    public function hasChildren(): bool
+    {
+        return true;
+    }
+
+    public function makeComponent()
+    {
+        $record = $this->getRecord();
+        if (! $record) {
+            throw new \Exception('Record not found');
+        }
+        $component = \Filament\Forms\Components\Fieldset::make($record->getAttribute('label'));
+        $this->makeColumns($component);
+        if ($this->getProps()->isNotEmpty()) {
+            $component->disabled(Utils::getBool($this->getProps()->get('disabled')));
+        }
+        $component->schema($this->makeChildren());
+        return $component;
+    }
+
+    public function getMainSchema(): array
+    {
+        return $this->extendCommonSchema([
+            \Filament\Forms\Components\Fieldset::make(__('Fieldset Details'))->statePath('props')
+                ->schema([
+                    \Filament\Forms\Components\Checkbox::make('disabled')->label(__('Disabled'))->default(false),
+                ]),
+        ]);
+    }
+
+    public function getValidationSchema(): array
+    {
+        return $this->extendValidationSchema();
+    }
+
+    public function getColumnsSchema(): array
+    {
+        return $this->extendColumnsSchema();
+    }
+}
