5c35c9a581cdf18ee83608b5838cc6c7d6b18fc8
Sam Maosa <smaosa@strathmore.edu>
2025-04-01 10:44:36 +0300

    New Feature: Markdown Editor - Added Support for the Markdown Editor Component - Closes #24

--- src/ComponentTypes/MarkdownEditor.php
+++ src/ComponentTypes/MarkdownEditor.php
@@ -2,4 +2,33 @@
 
 namespace Coolsam\VisualForms\ComponentTypes;
 
-class MarkdownEditor {}
+class MarkdownEditor extends RichEditor {
+    public function getOptionName(): string
+    {
+        return __('Markdown Editor');
+    }
+
+    public function letThereBe(string $name): \Filament\Forms\Components\MarkdownEditor
+    {
+        return \Filament\Forms\Components\MarkdownEditor::make($name);
+    }
+
+    protected function getToolbarButtons(): array
+    {
+        return collect([
+            'attachFiles',
+            'blockquote',
+            'bold',
+            'bulletList',
+            'codeBlock',
+            'heading',
+            'italic',
+            'link',
+            'orderedList',
+            'redo',
+            'strike',
+            'table',
+            'undo',
+        ])->mapWithKeys(fn($item) => [$item => str($item)->pascal()->snake()->title()->replace('_', ' ')])->toArray();
+    }
+}
